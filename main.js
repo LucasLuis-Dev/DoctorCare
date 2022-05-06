01997c29218c7c4696bd4b309c5bfcc8c38dc695
Lucas luis <101613654+Lucasluis1407@users.noreply.github.com>
2022-05-06 18:52:44 -0300

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,54 @@ onScroll()
 function onScroll() {
   showNavOnScroll()
   showBackToTopButtonOnScroll()
+
+  activateMenuAtCurrentSection(home)
+  activateMenuAtCurrentSection(services)
+  activateMenuAtCurrentSection(about)
+  activateMenuAtCurrentSection(contact)
 }
+
+function activateMenuAtCurrentSection(section) {
+  const targetLine = scrollY + innerHeight / 2
+
+  // Verificar se a seção passou da linha
+  // quais dados vou precisar?
+
+  // O topo da seção
+  const sectionTop = section.offsetTop
+
+  // A altura da seção
+  const sectionHeight = section.offsetHeight
+
+  // O topo da seção chegou ou ultrapassou a linha alvo
+  const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop
+
+  // Informações dos dados e da lógica
+  console.log('O topo da seção chegou ou passou da linha?', sectionTopReachOrPassedTargetLine)
+
+
+  // Verificar se a base está abaixo da linha alvo
+  // quais dados vou precisar?
+
+  // a seção termina onde?
+  const sectionEndsAt = sectionTop + sectionHeight
+
+  const sectionEndPassedTargetLine = sectionEndsAt <= targetLine
+
+  console.log('O fundo da seção passou da linha?', sectionTopReachOrPassedTargetLine)
+
+  // Limites da seção
+  const sectionBoundaries = sectionTopReachOrPassedTargetLine && !sectionEndPassedTargetLine
+
+  const sectionId = section.getAttribute('id')
+  const menuElemnt = document.querySelector(`.menu a[href*=${sectionId}]`)
+
+  menuElemnt.classList.remove('active')
+  if (sectionBoundaries) {
+    menuElemnt.classList.add('active')
+  }
+}
+
 
 function showNavOnScroll(){
   if (scrollY > 0) {
@@ -44,3 +91,5 @@ ScrollReveal({
  #about,
  #about header,
  #about .content`)
+
+
